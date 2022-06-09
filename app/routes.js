cbd28f79a5dcb8d8814137efecad61c4511f8b9a
OmerDWP <102356946+OmerDWP@users.noreply.github.com>
2022-06-09 08:32:58 +0100

    Add files via upload

--- app/routes.js
+++ app/routes.js
@@ -5,18 +5,99 @@ const router = express.Router()
 // Run this code when a form is submitted to 'juggling-balls-answer'
 router.post('/customer-answer', function (req, res) {
 
-    
+
     var passYes = req.session.data['customer_answer']
-    
+
+  console.log("passYes", passYes);
     // Check whether the variable matches a condition
     if (passYes == "Yes"){
       // Send user to next page
       res.redirect('success')
-    } else {
+
+    } else if (passYes == "No") {
       // Send user to ineligible page
       res.redirect('failure')
     }
-  
-  })
+})
+
+  router.post('/customer-answer1', function (req, res) {
+
+      var passYes = req.session.data['customer_answer1']
+
+    console.log("passYes", passYes);
+      // Check whether the variable matches a condition
+      if (passYes == "Yes"){
+        // Send user to next page
+        res.redirect('question_2')
+      } else if (passYes == "No") {
+        // Send user to ineligible page
+        res.redirect('question_2')
+      }
+
+    })
+
+    router.post('/customer-answer2', function (req, res) {
+
+        var passYes = req.session.data['customer_answer2']
+
+      console.log("passYes", passYes);
+        // Check whether the variable matches a condition
+        if (passYes == "Yes"){
+          // Send user to next page
+          res.redirect('Q3-Check-ID-at-Risk')
+        } else if (passYes == "No") {
+          // Send user to ineligible page
+          res.redirect('failure')
+        }
+
+      })
+
+      router.post('/customer-answer3', function (req, res) {
+
+          var passYes = req.session.data['customer_answer3']
+
+        console.log("passYes", passYes);
+          // Check whether the variable matches a condition
+          if (passYes == "Yes"){
+            // Send user to next page
+            res.redirect('failure')
+          } else if (passYes == "No") {
+            // Send user to ineligible page
+            res.redirect('Q4-Check-Date-of-Death')
+          }
+
+        })
+
+        router.post('/customer-answer4', function (req, res) {
+
+            var passYes = req.session.data['customer_answer4']
+
+          console.log("passYes", passYes);
+            // Check whether the variable matches a condition
+            if (passYes == "Yes"){
+              // Send user to next page
+              res.redirect('failure')
+            } else if (passYes == "No") {
+              // Send user to ineligible page
+              res.redirect('Q5-Check-Customer-History')
+            }
+
+          })
+
+          router.post('/customer-answer5', function (req, res) {
+
+              var passYes = req.session.data['customer_answer5']
+
+            console.log("passYes", passYes);
+              // Check whether the variable matches a condition
+              if (passYes == "Yes"){
+                // Send user to next page
+                res.redirect('success')
+              } else if (passYes == "No") {
+                // Send user to ineligible page
+                res.redirect('failure')
+              }
+
+            })
 
 module.exports = router
